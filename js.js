db5f00a854086097a51a3c18b7e710b1a6bb19e5
phuc-2512 <160859584+phuc-2512@users.noreply.github.com>
2024-02-29 22:13:24 +0700

    Update js.js
    lab2

--- js.js
+++ js.js
@@ -1,15 +1,65 @@
-const informations = {
-    name: "a",
-    language: [
-        "Anh",
-        {
-            certificate1: "Toeic",
-            certificate2: "Ielst",
-        },
-        "Hàn",
-    ],
-  age: 18
-}
+// console.log("So 1");
+// console.log("So 1");
+// console.log("So 1");
+// console.log("So 1");
 
-console.log(informations)
-console.log(informations.language[1].certificate2)
+// var i=1;
+
+// while (i<=1) {
+//     console.log(i);
+//     i++
+// }
+
+// // do {
+// //     console.log("1");
+// // } while (i<10)
+
+
+// for (let i=1; i<=10; i++) {
+//     console.log(i);
+// }
+
+// const nameStudent = [
+//     "Hai Dang",
+//     "Tran Nhi",
+//     "Tien Long",
+//     "Phuc",
+// ]
+// // var i=0;
+// // while (i<=nameStudent.length) {
+// //     console.log(nameStudent[i]);
+// //     i++;
+// // }
+
+// do {
+//     console.log(nameStudent[i]);
+//     i++;
+// } while (i<nameStudent.length)
+// console.log(nameStudent);
+// console.log(nameStudent[2]);
+
+// const classes = new Array (
+//     "JSB",
+//     ["JSB01", "JSB02"],
+//     "JSA",
+//     "JSI",
+// )
+// delete classes[2];
+// // console.log(classes.shift());
+// // console.log(classes.pop());
+// console.log(classes);
+// console.log(classes.length);
+// console.log(classes[1][1]);
+
+const informations = {
+    name: "Gia Han",
+    class: "JSA",
+    age: 18,
+    language: ["Anh",
+    {
+        certificate1: "Toeic",
+        certificated2: "Ielts",
+    }, "Han"]
+}
+console.log(informations);
+console.log(informations.language[1].certificated2);
