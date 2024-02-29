d5da1adcf6f54aa9013197ead4ea14e1e4d83596
phuc-2512 <160859584+phuc-2512@users.noreply.github.com>
2024-02-29 21:04:03 +0700

    Create js.js
    lab2

---
+++ js.js
@@ -0,0 +1,15 @@
+const informations = {
+    name: "a",
+    language: [
+        "Anh",
+        {
+            certificate1: "Toeic",
+            certificate2: "Ielst",
+        },
+        "Hàn",
+    ],
+  age: 18
+}
+
+console.log(informations)
+console.log(informations.language[1].certificate2)
